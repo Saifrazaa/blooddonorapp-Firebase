d1afc2086c17cf768fe0e728c16ccee9cdbd9b7a
root <razasaif171@gmail.com>
2018-02-03 15:28:02 +0500

    final results

--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,13 @@ function checkauthentication(req,res,next)
 router.get("/login",function(req,res,next){
   res.render("login",{title:"Login To Your Account"});
 });
-router.get('/donate-blood',checkauthentication, function(req, res, next) {
-  res.render('donate-blood', { title: "Donate Your Blood" });
+router.get('/donate-blood', function(req, res, next) {
+  res.render('data', { title: "Donate Your Blood" });
 });
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Sign Up To Get Start' });
 });
-router.get('/find-donors', function(req, res, next) {
+router.get('/find-donors',checkauthentication,function(req, res, next) {
   res.render('yourblood-group', { title: 'Blood Donors' });
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/signup', function(req, res, next) {
+  res.render("signup",{title:"Sign Up"});
+});
 module.exports = router;
